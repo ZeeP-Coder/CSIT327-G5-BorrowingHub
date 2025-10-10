d44c7187131542e58fcf2fa649fcd11aa40df99d
keeevys <kehrbymartinez@gmail.com>
2025-10-10 18:25:16 +0800

    changed UI in langing page and updated css

--- borrowinghub/settings.py
+++ borrowinghub/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'borrowinghub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': 'db.mbdcqebgoabagabgsjwc.supabase.co',
-        'PORT': '5432',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
